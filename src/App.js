7e439ab592c0d66c9e8813212631dc654c47b19f
Dmitry Kovalkov <dmitriy.k@kultprosvet.net>
2025-10-01 22:30:25 +0300

    161. Использование useEffect для Запросов

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 
 import JokeList from './components/JokeList';
 import './App.css';
@@ -23,7 +23,7 @@ function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  const fetchJokesHandler = async () => {
+  const fetchJokesHandler = useCallback(async () => {
     setIsLoading(true);
     setError(null);
 
@@ -41,7 +41,11 @@ function App() {
       setError(e.message);
     }
     setIsLoading(false);
-  };
+  }, []);
+
+  useEffect(() => {
+    fetchJokesHandler();
+  }, [fetchJokesHandler]);
 
   let content = <p>Шуток не найдено.</p>;
 
